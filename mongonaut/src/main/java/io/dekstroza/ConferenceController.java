b21c9b8832bfb01f6001c7f90be8044e8aec9b75
dekstroza <dejan.kitic@ericsson.com>
2019-03-05 15:10:29 +0000

    Simple test

--- mongonaut/src/main/java/io/dekstroza/ConferenceController.java
+++ mongonaut/src/main/java/io/dekstroza/ConferenceController.java
@@ -1,35 +1,35 @@
 package io.dekstroza;
 
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoCollection;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Post;
-import io.reactivex.Flowable;
-import io.reactivex.Single;
 
 @Controller("/mongonaut")
 public class ConferenceController {
 
-    private final MongoClient mongoClient;
+    //private final MongoClient mongoClient;
 
-    public ConferenceController(MongoClient mongoClient) {
-        this.mongoClient = mongoClient;
-    }
+    //public ConferenceController(MongoClient mongoClient) {
+    // this.mongoClient =mongoClient;
+    //}
+
+    //    @Get(value = "/conferences")
+    //    public Flowable<Conference> sayHelloWorld() {
+    //        return Flowable.fromPublisher(getCollection().find());
+    //
+    //    }
+    //
+    //    @Post("/conferences")
+    //    public Single<Conference> save(Integer id, String name) {
+    //        Conference conference = new Conference(id, name);
+    //        return Single.fromPublisher(getCollection().insertOne(conference)).map(success -> conference);
+    //    }
+    //
+    //    private MongoCollection<Conference> getCollection() {
+    //        return mongoClient.getDatabase("mongonaut").getCollection("conferences", Conference.class);
+    //    }
 
     @Get(value = "/conferences")
-    public Flowable<Conference> sayHelloWorld() {
-        return Flowable.fromPublisher(getCollection().find());
-
-    }
-
-    @Post("/conferences")
-    public Single<Conference> save(Integer id, String name) {
-        Conference conference = new Conference(id, name);
-        return Single.fromPublisher(getCollection().insertOne(conference)).map(success -> conference);
-    }
-
-    private MongoCollection<Conference> getCollection() {
-        return mongoClient.getDatabase("mongonaut").getCollection("conferences", Conference.class);
+    public String sayHelloWorld() {
+        return "Hello World";
     }
 }
